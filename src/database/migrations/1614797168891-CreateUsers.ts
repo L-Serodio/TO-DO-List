d11bb5ff7fd769145824d7917730fb04e87265a9
Lucas_Serodio <53245868+L-Serodio@users.noreply.github.com>
2021-03-03 15:57:20 -0300

    Initial settings

---
+++ src/database/migrations/1614797168891-CreateUsers.ts
@@ -0,0 +1,12 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateUsers1614797168891 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
