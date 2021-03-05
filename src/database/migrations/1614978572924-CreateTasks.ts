9eb0e51e45dcd565c9a890d6a2a722e47e313399
Lucas_Serodio <53245868+L-Serodio@users.noreply.github.com>
2021-03-05 18:12:42 -0300

    User's entity and migration

---
+++ src/database/migrations/1614978572924-CreateTasks.ts
@@ -0,0 +1,13 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateTasks1614978572924 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    
+    }
+
+}
